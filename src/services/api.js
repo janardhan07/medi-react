9c9eb2d17a3f931b7514b94adbb74a96ff3ddca8
janardhan07 <bjani07@gmail.com>
2026-05-25 22:36:43 -0400

    api js updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers['Authorization'] = `Bearer ${token}`
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
   return config
 })
 
